c671d8ab0055cb6f3a7cf369fc7e49a336385e80
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-04-03 00:22:37 +0200

    tests/handler: add tests

--- lib/copper/handler.ex
+++ lib/copper/handler.ex
@@ -12,8 +12,8 @@ defmodule Copper.Handler do
     Handler.send_json(conn, status, %{:message => message})
   end
 
-  def send_400(conn, error), do: send_message(conn, 400, error)
-  def send_400(conn), do: send_message(conn, 400, "Bad request")
+  def send_400(conn, error), do: Handler.send_message(conn, 400, error)
+  def send_400(conn), do: Handler.send_message(conn, 400, "Bad request")
 
   def send_403(conn) do
     Handler.send_message(conn, 403, "You're not allowed to perform this action")
---
+++ test/unit/handler_test.exs
@@ -0,0 +1,60 @@
+defmodule CopperTest.Handler do
+  use ExUnit.Case
+
+  alias Copper.Handler
+  alias Plug.Conn
+
+  import Dummy
+
+  test "send_json/3" do
+    dummy Conn, [
+      {"send_resp/3", :resp},
+      {"put_resp_content_type/2", :content_type}
+    ] do
+      dummy Jason, [{"encode!", :json}] do
+        assert Handler.send_json(:conn, 200, %{}) == :resp
+        assert called(Conn.put_resp_content_type(:conn, "application/json"))
+        assert called(Jason.encode!(%{}))
+        assert called(Conn.send_resp(:content_type, 200, :json))
+      end
+    end
+  end
+
+  test "send_message/3" do
+    dummy Handler, [{"send_json/3", :json}] do
+      assert Handler.send_message(:conn, 200, "hello") == :json
+      assert called(Handler.send_json(:conn, 200, %{:message => "hello"}))
+    end
+  end
+  test "send_400/2" do
+    dummy Handler, [{"send_message/3", :resp}] do
+      assert Handler.send_400(:conn, :error) == :resp
+      assert called(Handler.send_message(:conn, 400, :error))
+    end
+  end
+
+  test "send_400/1" do
+    dummy Handler, [{"send_message/3", :resp}] do
+      assert Handler.send_400(:conn) == :resp
+      assert called(Handler.send_message(:conn, 400, "Bad request"))
+    end
+  end
+
+  test "send_403/1" do
+    message = "You're not allowed to perform this action"
+
+    dummy Handler, [{"send_message/3", :resp}] do
+      assert Handler.send_403(:conn) == :resp
+      assert called(Handler.send_message(:conn, 403, message))
+    end
+  end
+
+  test "send_404/1" do
+    message = "The requested resource does not exist"
+
+    dummy Handler, [{"send_message/3", :resp}] do
+      assert Handler.send_404(:conn) == :resp
+      assert called(Handler.send_message(:conn, 404, message))
+    end
+  end
+end
