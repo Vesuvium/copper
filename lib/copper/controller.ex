07c9d02deed79bb500c95204e7387e746b3f6132
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-04-04 19:29:57 +0200

    feat/controller: add Controller module

---
+++ lib/copper/controller.ex
@@ -0,0 +1,38 @@
+defmodule Copper.Controller do
+  alias Ecto.{Changeset, Query, Schema}
+
+  @callback new(host :: String.t(), params :: %{}) ::
+              {:ok, Schema.t()} | {:error, Changeset.t()}
+
+  @callback get(host :: String.t(), params :: %{}) ::
+              {:ok, Schema.t()} | {:error, Changeset.t()}
+
+  @callback by_id(host :: String.t(), id :: String.t()) ::
+              {:ok, Schema.t()} | {:error, Query.CastError.t()}
+
+  @callback edit(String.t(), String.t(), params :: %{}) ::
+              {:ok, Schema.t()} | {:error, Changeset.t()}
+
+  @callback delete(host :: String.t(), id :: String.t()) ::
+              {integer(), nil | Query.CastError.t()}
+
+  defmacro __using__([]) do
+    quote do
+      alias unquote(
+              __CALLER__.module
+              |> Module.split()
+              |> List.replace_at(1, "Repo")
+              |> Module.concat()
+            )
+
+      alias unquote(
+              __CALLER__.module
+              |> Module.split()
+              |> List.replace_at(1, "Schemas")
+              |> Module.concat()
+            )
+
+      @behaviour Copper.Controller
+    end
+  end
+end
