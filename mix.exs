4c7e324235dbd3bbf081cbc0539ce31ee465ce11
Jacopo Cascioli <jacopo@jacopocascioli.com>
2020-04-04 20:27:24 +0200

    chore/mix: add package definition

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Copper.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -27,5 +29,19 @@ defmodule Copper.MixProject do
       {:plug, "~> 1.8"},
       {:plug_cowboy, "~> 2.1"}
     ]
+  end
+
+  defp package do
+    [
+      name: :copper,
+      files: ~w(mix.exs lib .formatter.exs README.md LICENSE),
+      maintainers: ["Jacopo Cascioli"],
+      licenses: ["MPL-2.0"],
+      links: %{"GitHub" => "https://github.com/Vesuvium/copper"}
+    ]
+  end
+
+  defp description do
+    "A library to build Plug-based APIs"
   end
 end
