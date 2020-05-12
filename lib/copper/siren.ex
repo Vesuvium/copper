1598e967b6b6ee1229d535a665c69dbdb69275c9
Jacopo Cascioli <jacopo@strangemachines.io>
2020-05-13 00:14:38 +0200

    feat/siren: add change_page/2

--- lib/copper/siren.ex
+++ lib/copper/siren.ex
@@ -1,4 +1,5 @@
 defmodule Copper.Siren do
+  alias Copper.Siren
   alias Plug.Conn
 
   @doc """
@@ -9,4 +10,14 @@ defmodule Copper.Siren do
     |> URI.merge(rhs)
     |> URI.to_string()
   end
+
+  @doc """
+  Creates an updated uri to a new page.
+  """
+  def change_page(conn, new_page) do
+    conn
+    |> Conn.request_url()
+    |> Siren.merge_uris("?" <> URI.encode_query(%{"page" => new_page}))
+  end
+
 end
--- test/unit/siren_test.exs
+++ test/unit/siren_test.exs
@@ -3,12 +3,26 @@ defmodule CopperTest.Siren do
   import Dummy
 
   alias Copper.Siren
+  alias Plug.Conn
 
   test "merge_uris/2" do
     dummy URI, [{"merge/2", :merge}, {"to_string", :uri}] do
       assert Siren.merge_uris("lhs", "rhs") == :uri
       assert called(URI.merge("lhs", "rhs"))
       assert called(URI.to_string(:merge))
+    end
+  end
+
+  test "change_page/2" do
+    dummy Conn, [{"request_url", "url"}] do
+      dummy URI, [{"encode_query", "query"}] do
+        dummy Siren, [{"merge_uris/2", :merge}] do
+          assert Siren.change_page(:conn, 2) == :merge
+          assert called(Conn.request_url(:conn))
+          assert called(URI.encode_query(%{"page" => 2}))
+          assert called(Siren.merge_uris("url", "?query"))
+        end
+      end
     end
   end
 end
