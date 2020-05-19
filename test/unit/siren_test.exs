7d7de8f70f8e8d2723b7e4796c11e00b89024ddf
Jacopo Cascioli <jacopo@strangemachines.io>
2020-05-19 22:51:33 +0200

    feat/siren: use Utils

--- test/unit/siren_test.exs
+++ test/unit/siren_test.exs
@@ -3,6 +3,7 @@ defmodule CopperTest.Siren do
   import Dummy
 
   alias Copper.Siren
+  alias Copper.Utils
   alias Plug.Conn
 
   test "merge_uris/2" do
@@ -27,50 +28,40 @@ defmodule CopperTest.Siren do
   end
 
   test "add_next/3" do
-    conn = %{query_params: %{"page" => "2"}}
     expected = [%{"rel" => "next", "href" => :page}]
 
-    dummy Siren, [{"change_page/2", :page}] do
-      assert Siren.add_next([], conn, 100) == expected
-      assert called(Siren.change_page(conn, 3))
-    end
-  end
-
-  test "add_next/3 without a given page" do
-    conn = %{query_params: %{}}
-
-    dummy Siren, [{"change_page/2", :page}] do
-      Siren.add_next([], conn, 100)
-      assert called(Siren.change_page(conn, 2))
+    dummy Utils, [{"get_page", 2}, {"get_items", 20}] do
+      dummy Siren, [{"change_page/2", :page}] do
+        assert Siren.add_next([], :conn, 100) == expected
+        assert called(Utils.get_page(:conn))
+        assert called(Utils.get_items(:conn))
+        assert called(Siren.change_page(:conn, 3))
+      end
     end
   end
 
   test "add_next/3 with items * page > count" do
-    conn = %{query_params: %{"items" => "200"}}
-
-    assert Siren.add_next([], conn, 100) == []
-  end
-
-  test "add_next/3 with items * page < count" do
-    conn = %{query_params: %{"items" => "1"}}
-    expected = [%{"rel" => "next", "href" => :page}]
-
-    dummy Siren, [{"change_page/2", :page}] do
-      assert Siren.add_next([], conn, 100) == expected
+    dummy Utils, [{"get_page", 1}, {"get_items", 200}] do
+      assert Siren.add_next([], :conn, 100) == []
     end
   end
 
   test "add_next/3 on last page" do
-    assert Siren.add_next([], %{query_params: %{"page" => "5"}}, 100) == []
+    dummy Utils, [{"get_page", 5}, {"get_items", 20}] do
+      assert Siren.add_next([], :conn, 100) == []
+    end
   end
 
   test "add_last/3" do
     conn = %{query_params: %{}}
     expected = [%{"rel" => "last", "href" => :page}]
 
-    dummy Siren, [{"change_page/2", :page}] do
-      assert Siren.add_last([], conn, 40) == expected
-      assert called(Siren.change_page(conn, 2))
+    dummy Utils, [{"get_items", 20}] do
+      dummy Siren, [{"change_page/2", :page}] do
+        assert Siren.add_last([], conn, 40) == expected
+        assert called(Utils.get_items(conn))
+        assert called(Siren.change_page(conn, 2))
+      end
     end
   end
 
