a508d5be0677882d23507bde6a67ed889d55fe4c
Jacopo Cascioli <jacopo@strangemachines.io>
2020-05-21 00:18:50 +0200

    feat/schema: generate @primary_key and @foreign_key_type declarations

--- lib/copper/schema.ex
+++ lib/copper/schema.ex
@@ -29,6 +29,10 @@ defmodule Copper.Schema do
 
       use Ecto.Schema
 
+      key_type = Application.get_env(:copper, :primary_key_type, :binary_id)
+      @primary_key {:id, key_type, autogenerate: true}
+      @foreign_key_type key_type
+
       @behaviour Copper.Schema
     end
   end
